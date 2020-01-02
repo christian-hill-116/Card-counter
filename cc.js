5f100b0c9539f3f4c38d9361b9ce29ddb44c2642
CHRISTIAN HILL <christian-hill-116@users.noreply.github.com>
2020-01-02 11:58:52 -0700

    init

---
+++ cc.js
@@ -0,0 +1,25 @@
+var count = 0;
+
+function cc(card) {
+  
+switch(card){
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count++;
+    break;
+  case 10:
+  case "J":
+  case "Q":
+  case "K":
+  case "A":
+    count--;
+    break;
+};
+
+if (count > 0){
+  return  count +" Bet";
+} return count + " Hold";
+}
